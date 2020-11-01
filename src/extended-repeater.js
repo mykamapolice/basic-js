df6a087d08e00c79227abab78cba85437bf5180f
Ilya Barachenia <ilya.barachenia@gmail.com>
2020-11-01 20:04:25 +0100

    fix: fixed tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,36 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    function repeater(str, options){
+    let full_string='';
+    if (options['repeatTimes']!==undefined){
+        for (let i=1; i<=options['repeatTimes']; i++){
+            full_string=full_string+str;
+            if (options['additionRepeatTimes']!==undefined){
+                for (let j=1; j<=options['additionRepeatTimes']; j++){
+                    full_string=full_string+options['addition'];
+                    if (j!==options['additionRepeatTimes']){
+                        if (options['additionSeparator']!==undefined) {
+                            full_string=full_string+options['additionSeparator'];
+                        }else{
+                            full_string=full_string+options['addition']+'|'
+                        }
+                    }
+
+                }
+            }
+            if (i!==options['repeatTimes']){
+                if (options['separator']!==undefined){
+                    full_string=full_string+options['separator']
+                }else{
+                    full_string=full_string+'+';
+                }
+            }
+
+        }
+    }
+    return full_string
+    }
 };
-  
+
+
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+     if (typeof (arr)!=='object'){
+        return 'Error, arr is not an object'
+    }
+    let new_arr=arr;
+    for (let i=0; i<new_arr.length; i++){
+        if (new_arr[i]==='--discard-next'){
+            new_arr.splice(i, 2)
+        }
+        if (new_arr[i]==='--discard-prev'){
+            new_arr.splice(i-1, 2)
+        }
+        if (new_arr[i]==='--double-next'){
+            new_arr.splice(i,1,arr[i+1])
+        }
+        if (new_arr[i]==='--double-prev'){
+            new_arr.splice(i,1,arr[i-1])
+        }
+    }
+    return new_arr
 };
